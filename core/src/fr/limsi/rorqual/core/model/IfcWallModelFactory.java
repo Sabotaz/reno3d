7d59132b00d7edeaafeef66aa9f5ad2d2bb52718
sablier <sablier@zendikar.fr>
2015-04-07 12:48:55 +0000

    fix sweptbody

--- core/src/fr/limsi/rorqual/core/model/IfcWallModelFactory.java
+++ core/src/fr/limsi/rorqual/core/model/IfcWallModelFactory.java
@@ -1,5 +1,6 @@
 package fr.limsi.rorqual.core.model;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.VertexAttributes;
@@ -11,17 +12,22 @@ import com.badlogic.gdx.graphics.g3d.utils.MeshPartBuilder;
 import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
+import com.badlogic.gdx.math.Matrix3;
 import com.badlogic.gdx.math.Matrix4;
 import com.badlogic.gdx.math.Vector3;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import javax.lang.model.type.PrimitiveType;
 
 import fr.limsi.rorqual.core.utils.IfcObjectPlacementUtils;
 import ifc2x3javatoolbox.ifc2x3tc1.ENUM;
+import ifc2x3javatoolbox.ifc2x3tc1.IfcArbitraryClosedProfileDef;
+import ifc2x3javatoolbox.ifc2x3tc1.IfcAxis2Placement3D;
 import ifc2x3javatoolbox.ifc2x3tc1.IfcCartesianPoint;
 import ifc2x3javatoolbox.ifc2x3tc1.IfcCircle;
+import ifc2x3javatoolbox.ifc2x3tc1.IfcCurve;
 import ifc2x3javatoolbox.ifc2x3tc1.IfcDirection;
 import ifc2x3javatoolbox.ifc2x3tc1.IfcDirectionSenseEnum;
 import ifc2x3javatoolbox.ifc2x3tc1.IfcExtrudedAreaSolid;
@@ -34,6 +40,7 @@ import ifc2x3javatoolbox.ifc2x3tc1.IfcMaterialSelect;
 import ifc2x3javatoolbox.ifc2x3tc1.IfcPolyline;
 import ifc2x3javatoolbox.ifc2x3tc1.IfcPositiveLengthMeasure;
 import ifc2x3javatoolbox.ifc2x3tc1.IfcProduct;
+import ifc2x3javatoolbox.ifc2x3tc1.IfcProfileDef;
 import ifc2x3javatoolbox.ifc2x3tc1.IfcRelAssociates;
 import ifc2x3javatoolbox.ifc2x3tc1.IfcRelAssociatesMaterial;
 import ifc2x3javatoolbox.ifc2x3tc1.IfcRepresentation;
@@ -168,6 +175,8 @@ public class IfcWallModelFactory {
 
     private Vector3 dir;
     private float depth;
+    private List<Vector3> sweptarea = new ArrayList<Vector3>();
+    private Matrix4 body_placement;
 
     private void setBody() {
 
@@ -180,6 +189,20 @@ public class IfcWallModelFactory {
             System.out.println("item: " + items[0]);
             if (items[0] instanceof IfcExtrudedAreaSolid) {
                 IfcExtrudedAreaSolid extrudedAreaSolid = (IfcExtrudedAreaSolid) items[0];
+                body_placement = IfcObjectPlacementUtils.toMatrix(extrudedAreaSolid.getPosition());
+                IfcProfileDef profile = extrudedAreaSolid.getSweptArea();
+                System.out.println("profile: " + profile.getProfileType()); // should be AREA :o
+                if (profile instanceof IfcArbitraryClosedProfileDef) {
+                    IfcArbitraryClosedProfileDef arbitraryClosedProfileDef = (IfcArbitraryClosedProfileDef) profile;
+                    IfcCurve curve = arbitraryClosedProfileDef.getOuterCurve();
+                    System.out.println("curve: " + curve);
+                    if (curve instanceof IfcPolyline) {
+                        IfcPolyline polyline = (IfcPolyline) curve;
+                        for (IfcCartesianPoint point : polyline.getPoints()) {
+                            sweptarea.add(IfcObjectPlacementUtils.toVector(point));
+                        }
+                    }
+                }
                 dir = IfcObjectPlacementUtils.toVector(extrudedAreaSolid.getExtrudedDirection().getDirectionRatios());
                 depth = (float)extrudedAreaSolid.getDepth().value;
 
@@ -206,19 +229,25 @@ public class IfcWallModelFactory {
             node.id = "layer"+i;
 
             float thickness = materialLayersThickness.get(i);
-
-            meshBuilder = builder.part("part"+i, GL20.GL_LINES, VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal, new Material());
+            System.out.println(thickness);
+            meshBuilder = builder.part("part"+i, GL20.GL_LINES, VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal, new Material(ColorAttribute.createDiffuse(Color.YELLOW)));
             //meshBuilder.line(start, end);
             Vector3 corner000 = start.cpy().add(layerOffset);
             Vector3 corner001 = start.cpy().add(layerOffset).add(XY.cpy().scl(direction * thickness));
             Vector3 corner010 = end.cpy().add(layerOffset).add(XY.cpy().scl(direction * thickness));
             Vector3 corner011 = end.cpy().add(layerOffset);
+            meshBuilder.rect(corner000, corner001, corner010, corner011, Z);
+            layerOffset.add(XY.cpy().scl(direction * thickness));
+        }
+        meshBuilder = builder.part("sweptsolid", GL20.GL_LINES, VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal, new Material(ColorAttribute.createDiffuse(Color.WHITE)));
+        meshBuilder.setVertexTransform(body_placement);
+        for (int i = 0; i < sweptarea.size(); i++) {
             Vector3 z_shape = dir.cpy().scl(depth);
-            Vector3 corner100 = corner000.cpy().add(z_shape);
-            Vector3 corner101 = corner001.cpy().add(z_shape);
-            Vector3 corner110 = corner010.cpy().add(z_shape);
-            Vector3 corner111 = corner011.cpy().add(z_shape);
-            meshBuilder.box(corner000, corner001, corner011, corner010, corner100, corner101, corner111, corner110);
+            Vector3 p1 = sweptarea.get(i);
+            Vector3 p2 = sweptarea.get((i + 1) % sweptarea.size());
+            Vector3 p3 = p1.cpy().add(z_shape);
+            Vector3 p4 = p2.cpy().add(z_shape);
+            meshBuilder.rect(p1, p2, p4, p3, Z);
         }
         model = builder.end();
     }
