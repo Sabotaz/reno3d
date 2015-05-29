5d28661d8d0cf44f5bafb8c5f1633291ab864a06
Thomas Ricordeau <thomas.ricordeau@limsi.fr>
2015-05-29 15:48:30 +0000

    DesktopLauncher update

--- desktop/src/fr/limsi/rorqual/desktop/DesktopLauncher.java
+++ desktop/src/fr/limsi/rorqual/desktop/DesktopLauncher.java
@@ -9,15 +9,6 @@ import fr.limsi.rorqual.core.model.IfcHolder;
 import fr.limsi.rorqual.core.model.IfcHelper;
 import fr.limsi.rorqual.core.view.MainApplicationAdapter;
 
-import ifc2x3javatoolbox.ifc2x3tc1.IfcBuildingStorey;
-import ifc2x3javatoolbox.ifc2x3tc1.IfcCartesianPoint;
-import ifc2x3javatoolbox.ifc2x3tc1.IfcDoor;
-import ifc2x3javatoolbox.ifc2x3tc1.IfcLengthMeasure;
-import ifc2x3javatoolbox.ifc2x3tc1.IfcOpeningElement;
-import ifc2x3javatoolbox.ifc2x3tc1.IfcWallStandardCase;
-import ifc2x3javatoolbox.ifc2x3tc1.IfcWindow;
-import ifc2x3javatoolbox.ifc2x3tc1.LIST;
-import ifc2x3javatoolbox.ifc2x3tc1.SET;
 import ifc2x3javatoolbox.ifcmodel.IfcModel;
 
 public class DesktopLauncher {
@@ -28,22 +19,10 @@ public class DesktopLauncher {
 
         IfcHelper.initialiseIfcModel(ifcModel);
         IfcHelper.createApartmentTest(ifcModel);
-//        IfcCartesianPoint pointA = IfcHelper.createCartesianPoint2D(0.0,0.0);
-//        IfcCartesianPoint pointB = IfcHelper.createCartesianPoint2D(500,200);
-//        IfcHelper.addWall(ifcModel,"1st floor","wall",pointA,pointB,18);
-//        IfcWallStandardCase wall = IfcHelper.getWall(ifcModel,"wall");
-//        IfcHelper.addDoubleWindow(ifcModel,"window",wall,100,150,200,50);
-
         IfcHelper.saveIfcModel(ifcModel);
 
-//        try {
-//            IfcHolder.getInstance().openModel(new File("data/ifc/3etage12murs3slabs12openings.ifc"));
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-
         try {
-            IfcHolder.getInstance().openModel(new File("C:\\Users\\ricordeau\\Desktop\\Coucou.ifc"));
+            IfcHolder.getInstance().openModel(new File("data/ifc/coucou.ifc"));
         } catch (Exception e) {
             e.printStackTrace();
         }
