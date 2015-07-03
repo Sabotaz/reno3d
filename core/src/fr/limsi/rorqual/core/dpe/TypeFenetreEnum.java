e6a2c99fd7cdc950124c008715e205e0f511cd67
Thomas Ricordeau <thomas.ricordeau@limsi.fr>
2015-07-03 13:54:00 +0000

    On est reparti pour tout recoder !!!!

---
+++ core/src/fr/limsi/rorqual/core/dpe/TypeFenetreEnum.java
@@ -0,0 +1,10 @@
+package fr.limsi.rorqual.core.dpe;
+
+/**
+ * Created by ricordeau on 03/07/15.
+ */
+public enum TypeFenetreEnum {
+    BATTANTE,
+    COULISSANTE,
+    TOIT;
+}
---
+++ core/src/fr/limsi/rorqual/core/dpe/TypeVitrageEnum.java
@@ -0,0 +1,13 @@
+package fr.limsi.rorqual.core.dpe;
+
+/**
+ * Created by ricordeau on 03/07/15.
+ */
+public enum TypeVitrageEnum {
+    SIMPLE_VITRAGE,
+    SURVITRAGE,
+    DOUBLE_VITRAGE_INF_1990,
+    DOUBLE_VITRAGE_SUP_1990_INF_2001,
+    DOUBLE_VITRAGE_SUP_2001,
+    TRIPLE_VITRAGE;
+}
