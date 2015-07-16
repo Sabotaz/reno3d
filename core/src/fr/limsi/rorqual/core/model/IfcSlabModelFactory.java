7dba9e8611cf5d8744e4f647b9762ebda31e5410
sablier <sablier@zendikar.fr>
2015-07-16 11:16:25 +0000

    fix texture shaders (add UV scale) and add textures to slabs

--- core/src/fr/limsi/rorqual/core/model/IfcSlabModelFactory.java
+++ core/src/fr/limsi/rorqual/core/model/IfcSlabModelFactory.java
@@ -1,9 +1,11 @@
 package fr.limsi.rorqual.core.model;
 
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g3d.Material;
 import com.badlogic.gdx.graphics.g3d.Model;
 import com.badlogic.gdx.graphics.g3d.ModelInstance;
+import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
 import com.badlogic.gdx.graphics.g3d.attributes.TextureAttribute;
 import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
@@ -16,6 +18,7 @@ import java.util.List;
 import eu.mihosoft.vrl.v3d.CSG;
 import eu.mihosoft.vrl.v3d.Vertex;
 import fr.limsi.rorqual.core.model.primitives.ExtrudedAreaSolidModel;
+import fr.limsi.rorqual.core.model.primitives.MaterialTypeEnum;
 import fr.limsi.rorqual.core.model.primitives.PolylineModel;
 import fr.limsi.rorqual.core.model.primitives.TrimmedCurveModel;
 import fr.limsi.rorqual.core.utils.AssetManager;
@@ -67,6 +70,7 @@ public class IfcSlabModelFactory {
         this.setBody();
         this.setOpenings();
         this.mergeOpenings();
+        this.setMaterials();
     }
 
     private List<ModelProvider> opening_models = new ArrayList<ModelProvider>();
@@ -110,6 +114,7 @@ public class IfcSlabModelFactory {
     }
 
     private ArrayList<Float> materialLayersThickness = new ArrayList<Float>();
+    private ArrayList<MaterialTypeEnum> materialLayersMaterials = new ArrayList<MaterialTypeEnum>();
     private float offset = 0;
     private int direction = 1;
 
@@ -130,8 +135,11 @@ public class IfcSlabModelFactory {
                     offset = (float)offset_measure.value;
                     LIST<IfcMaterialLayer> layers = layerSet.getMaterialLayers();
 
-                    for (IfcMaterialLayer layer : layers)
-                        materialLayersThickness.add((float)layer.getLayerThickness().value);
+                    for (IfcMaterialLayer layer : layers) {
+                        materialLayersThickness.add((float) layer.getLayerThickness().value);
+                        MaterialTypeEnum material = MaterialTypeEnum.valueOf(layer.getMaterial().getName().getDecodedValue());
+                        materialLayersMaterials.add(material);
+                    }
 
                     if (layerSetDirection.value.name().equals("AXIS2")) {
                         // OK
@@ -173,6 +181,49 @@ public class IfcSlabModelFactory {
         }
     }
 
+    Material frontMaterial = new Material();
+    Material backMaterial = new Material();
+
+    private void setMaterials() {
+        if (materialLayersMaterials.size() > 0) {
+            Texture texture1_diff = materialLayersMaterials.get(0).getDiffuse();
+            Texture texture1_norm = materialLayersMaterials.get(0).getNormal();
+
+            Texture texture2_diff = materialLayersMaterials.get(materialLayersMaterials.size()-1).getDiffuse();
+            Texture texture2_norm = materialLayersMaterials.get(materialLayersMaterials.size()-1).getNormal();
+
+            texture1_diff.setWrap(Texture.TextureWrap.Repeat,Texture.TextureWrap.Repeat);
+            texture2_diff.setWrap(Texture.TextureWrap.Repeat,Texture.TextureWrap.Repeat);
+
+            TextureAttribute ta1_diff = TextureAttribute.createDiffuse(texture1_diff);
+            TextureAttribute ta2_diff = TextureAttribute.createDiffuse(texture2_diff);
+
+            ta1_diff.scaleU = ta1_diff.scaleV = 0.25f;
+            ta2_diff.scaleU = ta2_diff.scaleV = 0.25f;
+            frontMaterial.set(ta1_diff);
+            backMaterial.set(ta2_diff);
+
+            if (texture1_norm != null) {
+                texture1_norm.setWrap(Texture.TextureWrap.Repeat,Texture.TextureWrap.Repeat);
+                TextureAttribute ta1_norm = TextureAttribute.createNormal(texture1_norm);
+                ta1_norm.scaleU = ta1_norm.scaleV = 0.25f;
+                frontMaterial.set(ta1_norm);
+            }
+
+            if (texture2_norm != null) {
+                texture2_norm.setWrap(Texture.TextureWrap.Repeat,Texture.TextureWrap.Repeat);
+                TextureAttribute ta2_norm = TextureAttribute.createNormal(texture2_norm);
+                ta2_norm.scaleU = ta2_norm.scaleV = 0.25f;
+                backMaterial.set(ta2_norm);
+            }
+
+        }
+        else {
+            frontMaterial.set(ColorAttribute.createDiffuse(Color.WHITE));
+            backMaterial.set(ColorAttribute.createDiffuse(Color.WHITE));
+        }
+    }
+
 
     private ModelProvider wall_model;
     private CSG wall_csg;
@@ -197,17 +248,7 @@ public class IfcSlabModelFactory {
 
     public ModelInstance getModel() {
         //return wall_model.getModel();
-        Model model = CSGUtils.toModel(wall_csg);
-
-        Texture diffuse = (Texture) AssetManager.getInstance().get("wood-floorboards-texture");
-
-        diffuse.setWrap(Texture.TextureWrap.Repeat, Texture.TextureWrap.Repeat);
-
-        TextureAttribute tad = TextureAttribute.createDiffuse(diffuse);
-
-        Material material = model.materials.get(0);
-
-        material.set(tad);
+        Model model = CSGUtils.toModel(wall_csg, frontMaterial, backMaterial);
 
         return new ModelInstance(model);
     }
