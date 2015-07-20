c2f9bb1cdb34c3923bba84c75a7857ed609f580d
sablier <sablier@zendikar.fr>
2015-07-20 14:26:16 +0000

    add getters and setters for murs

--- core/src/fr/limsi/rorqual/core/model/Mur.java
+++ core/src/fr/limsi/rorqual/core/model/Mur.java
@@ -7,9 +7,69 @@ import com.badlogic.gdx.math.Vector3;
  */
 public class Mur {
 
-    private Vector3 A;
-    private Vector3 B;
+    private Vector3 A = new Vector3();
+    private Vector3 B = new Vector3();
 
-    private float height, width, depth;
+    private float height;
+    private float width;
+    private float depth;
 
+    private static float DEFAULT_DEPTH = 0.2f;
+    private static float DEFAULT_HEIGHT = 2.8f;
+
+    public Mur(Vector3 a, Vector3 b) {
+        this(a, b, DEFAULT_DEPTH, DEFAULT_HEIGHT);
+    }
+
+    public Mur(Vector3 a, Vector3 b, float d) {
+        this(a,b,d, DEFAULT_HEIGHT);
+    }
+
+    public Mur(Vector3 a, Vector3 b, float d, float h) {
+        this.A = new Vector3(a);
+        this.B = new Vector3(b);
+        this.height = h;
+        this.depth = d;
+        this.width = b.cpy().sub(a).len();
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
+    public float getWidth() {
+        return width;
+    }
+
+    private void setWidth(float width) { // PRIVATE ! changed programatically
+        this.width = width;
+    }
+
+    public float getDepth() {
+        return depth;
+    }
+
+    public void setDepth(float depth) {
+        this.depth = depth;
+    }
+
+    public Vector3 getA() {
+        return A;
+    }
+
+    public void setA(Vector3 a) {
+        A = a;
+    }
+
+    public Vector3 getB() {
+        return B;
+    }
+
+    public void setB(Vector3 b) {
+        B = b;
+    }
 }
