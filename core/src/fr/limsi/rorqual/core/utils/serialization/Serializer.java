6cb4ed08fce965163aac914cab1a6604bc4ee503
sablier <sablier@zendikar.fr>
2016-01-11 09:18:11 +0000

    auto-detect annotations

--- core/src/fr/limsi/rorqual/core/utils/serialization/Serializer.java
+++ core/src/fr/limsi/rorqual/core/utils/serialization/Serializer.java
@@ -3,6 +3,7 @@ package fr.limsi.rorqual.core.utils.serialization;
 import com.thoughtworks.xstream.XStream;
 
 import fr.limsi.rorqual.core.model.Batiment;
+import fr.limsi.rorqual.core.model.ModelHolder;
 
 /**
  * Created by christophe on 08/01/16.
@@ -11,7 +12,8 @@ public class Serializer {
 
     public static void saveAll() {
         XStream stream = new XStream();
-        stream.processAnnotations(Batiment.class);
+        stream.autodetectAnnotations(true);
+        System.out.println(stream.toXML(ModelHolder.getInstance().getBatiment()));
     }
 
 }
