4092f96bf70e8b7a56a0665ae4348af9d9db85d9
sablier <sablier@zendikar.fr>
2016-01-18 09:03:21 +0000

    remove unused files

--- android/assets/data/models/scripts/lanceBlender.py
+++
@@ -1,6 +0,0 @@
-import os
-
-for path, dirs, files in os.walk("."):
-	for filename in files:
-		if filename.endswith('.obj'):
-			os.system("blender --background projetVide.blend --python objToObjWithNormals.py -- " + os.path.join(path,filename))
