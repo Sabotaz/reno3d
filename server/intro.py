4bdbf1f3dbf23c0ee9863320dd83300767537019
sablier <sablier@zendikar.fr>
2016-04-11 14:06:01 +0200

    setup intro

--- server/intro.py
+++ server/intro.py
@@ -5,8 +5,11 @@ import cgi
 form = cgi.FieldStorage()
 print("Content-type: text/html; charset=utf-8\n")
 
-print(form.getvalue("name"))
-
 html = open("html/intro.html").read()
+config = open("../android/assets/data/misc/config.properties").read()
+config = {c[0] : c[1] for c in [l.split("=") for l in config.split("\n")]}
+intro = config["INTRO#"+config["VERSION"]].replace("\\n","<br/>")
+
+html = html.replace("$INTRO", intro)
 
 print(html)
