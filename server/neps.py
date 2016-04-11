cad092d62af1f7552b00fe35d1eddbc5faffdb30
sablier <sablier@zendikar.fr>
2016-04-11 17:52:58 +0200

    add neps

--- server/neps.py
+++ server/neps.py
@@ -5,7 +5,7 @@ import cgi
 form = cgi.FieldStorage()
 print("Content-type: text/html; charset=utf-8\n")
 
-print(form.getvalue("name"))
+#print(form.getvalue("name"))
 
 html = open("html/neps.html").read()
 
