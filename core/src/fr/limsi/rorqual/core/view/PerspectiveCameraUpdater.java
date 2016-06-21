a345497127c19aeed33c8cc5f4040f0142cd1fc0
sablier <sablier@zendikar.fr>
2016-06-21 09:45:23 +0200

    probably fix mouse

--- core/src/fr/limsi/rorqual/core/view/PerspectiveCameraUpdater.java
+++ core/src/fr/limsi/rorqual/core/view/PerspectiveCameraUpdater.java
@@ -272,6 +272,7 @@ public class PerspectiveCameraUpdater extends CameraUpdater {
 
     @Override
     public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        translating = !(translating && button == 1);
         return false;
     }
 
